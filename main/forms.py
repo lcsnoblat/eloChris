4fc78f2123aeaf8d69ae3da65977d0979fbf96e1
Lucas Noblat <lucasnoblat123@gmail.com>
2018-01-11 04:57:02 -0200

    Adição de estatisticas

--- main/forms.py
+++ main/forms.py
@@ -10,23 +10,32 @@ class EloForm(forms.Form):
     Choices = (
         ('Sim', 'Sim'), ('Não', 'Não'))
 
-    vitoria = forms.ChoiceField(choices=Choices, widget=forms.Select(attrs={'class':'select'}))
-    pontos = forms.IntegerField(label='Pontos')
+    vitoria = forms.ChoiceField(label='Você ganhou o jogo?', choices=Choices, widget=forms.Select(attrs={'class':'select'}))
+    pontos = forms.IntegerField(label='Pontos de não-rage')
 
     def update_elo(form, usuario):
         vitoria = form.cleaned_data['vitoria']
         pontos = form.cleaned_data['pontos']
 
+        print(vitoria)
+
         if pontos + usuario.pontos >= 100:
             proximo_elo = Elo.objects.get(rank = usuario.elo.rank - 1)
             usuario.elo = proximo_elo
             usuario.pontos = (pontos + usuario.pontos) - 100
+            if vitoria == 'Sim':
+                usuario.partidas_ganhas = usuario.partidas_ganhas + 1
+            else:
+                usuario.partidas_perdidas = usuario.partidas_perdidas + 1
+            usuario.partidas_jogadas = usuario.partidas_jogadas + 1
             usuario.save()
         else:
             pontos_totais = pontos + usuario.pontos
             usuario.pontos = pontos_totais
+            if vitoria == 'Sim':
+                usuario.partidas_ganhas = usuario.partidas_ganhas + 1
+            else:
+                usuario.partidas_perdidas = usuario.partidas_perdidas + 1
+            usuario.partidas_jogadas = usuario.partidas_jogadas + 1
             usuario.save()
-
-
-
 
--- main/models.py
+++ main/models.py
@@ -3,13 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Elo(models.Model):
-    # Elos = (
-    #     ('BRNZ5', 'Bronze 5'),
-    #     ('BRNZ4', 'Bronze 4'),
-    #     ('BRNZ3', 'Bronze 3'),
-    #     ('BRNZ2', 'Bronze 2'),
-    #     ('BRNZ1', 'Bronze 1')
-    # )
     nome = models.CharField(max_length=100)
     image =  models.CharField(max_length=1000)
     criado = models.DateTimeField(auto_now_add=True)
@@ -24,6 +17,9 @@ class PontosUsuario(models.Model):
     nome = models.CharField(max_length=100)
     pontos = models.IntegerField()
     elo = models.ForeignKey(Elo, related_name='elo_usuario')
+    partidas_jogadas = models.IntegerField()
+    partidas_ganhas = models.IntegerField()
+    partidas_perdidas = models.IntegerField()
 
     def __str__(self):
         return self.nome
